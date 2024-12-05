389ccf9a5ec336b19d27ad8012acfd0f12f5b60c
ash <123100292+ashmor23@users.noreply.github.com>
2024-12-04 19:22:00 -0600

    Update server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const { createServer } = require('node:http');
+const fs = require('fs');
+
+const express = require("express");
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+   res.statusCode = 200;
+   res.setHeader('Content-Type', 'text/html');
+   fs.readFile('index.html', function(error, data){
+      if (error) {
+         res.writeHead(404);
+         res.write('Error: file not found');
+      }else{
+         res.write(data);
+      }
+     res.end();
+   });
+});
+
+server.listen(port, hostname, () => {
+   console.log(`Server running at http://${hostname}:${port}/`);
+});
