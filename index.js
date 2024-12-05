c946b59929a7b33585de60a253ba68e88c233887
ash <123100292+ashmor23@users.noreply.github.com>
2024-12-04 19:25:35 -0600

    Update index.js
    just some bare bones from my test app

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// for login form functionality 
+// for login form functionality and ??
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
