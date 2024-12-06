c7a580cfa261cd4b5646afaa84062d64b7d06161
evan52703 <128087397+evan52703@users.noreply.github.com>
2024-12-06 15:15:55 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Login from "./components/Login";
 import BookList from './components/BookList';
+import BookInfo from './components/BookInfo';
 import './index.css';
 
 function App() {
