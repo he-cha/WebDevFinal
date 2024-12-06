b3a02a3b6c148133154e22e62bb25f53591ca6fd
evan52703 <128087397+evan52703@users.noreply.github.com>
2024-12-06 15:51:39 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Login />} /> 
                     <Route path="/booklist" element={<BookList />} />
-                    <Route path="/booklist/bookinfo" element={<BookInfo />} />
+                    <Route path="/booklist/bookinfo/:title" element={<BookInfo />} />
                 </Routes>
             </div>
         </Router>
