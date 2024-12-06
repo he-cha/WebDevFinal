55ec850e2e7e2c5ff99dab5b5e000d59610f20d9
ash <123100292+ashmor23@users.noreply.github.com>
2024-12-06 16:17:20 -0600

    Update Login.js
    imported useNavigate

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const [username, setUserName] = useState("");    // This manages the username input
