75a1c0c03eb379415c455f36277e3e6993131c09
ash <123100292+ashmor23@users.noreply.github.com>
2024-12-05 19:32:14 -0600

    Update index.js
    changed & clarified comments

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // uses react to render components like <App /> 
-// but App.js doesn't exist yet
+// (but App.js doesn't exist yet)
+
+/*
+will eventually initialize express and .ejs in this file 
+s.t. react components can be imported to & rendered from here
+*/
+
+/* 
+may need to change .render() code below: 
+does root.render here assume it's rendering into .html?
+    if so, then change this file s.t. it renders to index.ejs
+    in a .ejs-friendly way (or change <div id="root"> in index.ejs)
+*/
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -13,29 +25,3 @@ root.render(
         <App />
     </React.StrictMode>
 );
-
-/*
-// gets book list from db and renders book info table in index.ejs 
-TO DO: put code in server.js since express and ejs are already initialized there
-    ++ change destination from index.ejs to BookList.js component file
-
-       ? export BookList from BookList.js straight into index.ejs
-       OR use server.js to import BookList.js and render into index.ejs ???
-
-// TO DO: store books from db into this list
-const books = []
-
-app.get('/home', (req, res) => {
-    // arg in braces is object to send to ejs file
-    res.render('index.ejs', { books: books });
-})
-
-
-// redundant code that's also in server.js:
-import express from 'express';
-const app = express();
-app.set('view engine', 'ejs');
-app.listen(3000, function (req, res) { 
-    console.log("Connected on port:3000"); 
-});
-*/
